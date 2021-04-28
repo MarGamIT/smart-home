4cee0794ac8f6cdc7e726beff94de243ace42ce1
DominikW <dominik.wukovatz@gmail.com>
2021-04-28 14:41:28 +0200

    Initilised core components [0] added various components [1] added diverse services [2] added various guards

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { ControlpanelComponent } from './components/controlpanel/controlpanel.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { AdminComponent } from './components/admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegistrationComponent,
+    ControlpanelComponent,
+    PageNotFoundComponent,
+    NavigationComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
